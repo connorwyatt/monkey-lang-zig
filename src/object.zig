96c837c1c8d403e96f49c8d882255d6af4348368
Connor Wyatt <connorwyatt1@gmail.com>
2025-03-21 23:06:45 +0000

    feat: add value representation for integers

---
+++ src/object.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+pub const ObjectType = struct {
+    pub const INTEGER_OBJ = "INTEGER";
+};
+
+pub const Object = struct {
+    subtype: *const Subtype,
+
+    const Self = @This();
+
+    pub const Subtype = union(enum) { integer: Integer };
+
+    pub fn @"type"(self: *const Self) []const u8 {
+        return switch (self.subtype) {
+            inline else => |x| x.type(),
+        };
+    }
+
+    pub fn inspect(self: *const Self, allocator: Allocator) []const u8 {
+        return switch (self.subtype) {
+            inline else => |x| x.inspect(allocator),
+        };
+    }
+};
+
+pub const Integer = struct {
+    value: i64,
+
+    const Self = @This();
+
+    pub fn @"type"(self: *const Self) []const u8 {
+        _ = self;
+        return ObjectType.INTEGER_OBJ;
+    }
+
+    pub fn inspect(
+        self: *const Self,
+        allocator: Allocator,
+    ) Allocator.Error![]const u8 {
+        return try std.fmt.allocPrint(allocator, "{}", .{self.value});
+    }
+};
